0b7f9bbdfbde42976e73929d44f47e86099e55fa
Theofilusarifin <theofilusarifin@gmail.com>
2022-10-02 13:09:29 +0700

    Update proses so user can clear the schedule

--- proses_update_jadwal.php
+++ proses_update_jadwal.php
@@ -6,20 +6,20 @@ $jadwal = new Jadwal("localhost", "root", "", "project_uts");
 
 // Security for post method 
 if (isset($_POST['submit'])) {
-    if (!empty($_POST['checkbox_jadwal'])) {
         // Define selected mahasiswa nrp
         $nrp = $_POST['nrp'];
         // Delete all previous selected mahasiswa jadwal
         $jadwal->DeleteJadwal($nrp);
         // Input all new many to many jadwal data
-        foreach ($_POST['checkbox_jadwal'] as $value) {
-            $data = explode("_", $value);
-            $idhari = $data[0];
-            $idjam_kuliah = $data[1];
-            $jadwal->InsertJadwal($nrp, $idjam_kuliah, $idhari);
+        if(!empty($_POST['checkbox_jadwal'])){
+            foreach ($_POST['checkbox_jadwal'] as $value) {
+                $data = explode("_", $value);
+                $idhari = $data[0];
+                $idjam_kuliah = $data[1];
+                $jadwal->InsertJadwal($nrp, $idjam_kuliah, $idhari);
+            }
         }
         // After finished, redirect to index
         header("Location: index.php");
-    }
 }
 ?>
